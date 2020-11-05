f649320a00136efca6e28a93a6bb52eb7bb6a4d2
chandancodes <chandacodes@gmail.com>
2020-11-05 22:58:56 +0530

    2

---
+++ for-loop.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+    int i=0;
+    for(i=0;i<10;i++){
+        printf("i = %d\n", i);
+    } printf("%d is not of this loop\n", i);
+return 0;
+}
---
+++ while-loop.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main(){
+    int i=0;
+    while(i<10){
+        printf("i=%d\n", i);
+        i++;
+    } printf("%d is not a part of this loop\n", i);
+return 0;
+}
