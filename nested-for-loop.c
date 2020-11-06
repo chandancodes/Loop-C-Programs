2cfdfe8cbf3c030645448b66d80dfa115f23374e
chandancodes <chandacodes@gmail.com>
2020-11-06 19:32:19 +0530

    Create nested-for-loop.c

---
+++ nested-for-loop.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        printf("Hi\n");
+        for(j=0;j<5;j++){
+            printf("Hello\n");
+        }
+    }
+return 0;
+}
